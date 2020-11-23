79bcc61f99c62951eadc609d271e564d19f2ceb6
Lascau Ionut Sebastian <lascauionutsebastian98@gmail.com>
2020-11-23 17:58:14 +0200

    added error icon and started doing merge button to open the output directory

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import os
 import glob
 from tkinter import filedialog as fd
 import shutil
+import pandas as pd
 
 LARGE_FONT= ("Verdana", 12)
 NORM_FONT = ("Helvetica", 10)
@@ -15,6 +16,7 @@ def popup_msg(message):
         popup.destroy()
 
     popup.wm_title("Error")
+    popup.iconbitmap(r"error_icon.ico")
     label = tk.Label(popup, text = message, font = NORM_FONT)
     label.pack(side = "top", fill = "x", pady = 10)
     B1 = tk.Button(popup, text = "Ok", command = popup.destroy)
@@ -44,7 +46,21 @@ def clear():
         filelist = glob.glob(os.path.join('input_csv', "*.csv"))
         for f in filelist:
             os.remove(f)
-        
+
+def merge():
+    if not os.path.exists('input_csv') or  not os.listdir('input_csv'):
+        popup_msg('No csv files to merge...')
+    else:
+        columns_list = ['test']
+        columns_names_list = ['ana']
+        current_directory = os.getcwd()
+        output_directory = os.path.join(current_directory, r'output_csv')
+        if not os.path.exists(output_directory):
+            os.makedirs(output_directory)
+        data = pd.DataFrame(zip(*columns_list),
+                            columns = columns_names_list)
+        data.to_csv('output_csv/output.csv')
+        os.startfile(os.getcwd() + '/output_csv')
 
 root = Tk()
 root.title('Csv merger')
@@ -60,7 +76,6 @@ root.geometry('750x500')
 frame = tk.Frame(root)
 frame.pack()
 
-
 add_button = tk.Button(frame,
                        text = 'Add csv',
                        command = add_csv)
@@ -71,7 +86,11 @@ clear_button = tk.Button(frame,
                          command = clear)
 clear_button.pack()
 
+merge_button = tk.Button(frame,
+                         text = 'Merge',
+                         command = merge)
 
+merge_button.pack()
 
 
 root.mainloop()
