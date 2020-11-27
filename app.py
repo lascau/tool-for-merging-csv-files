8369ec916ffa4d818551460ae13b9f15fc9c3c45
Lascau Ionut Sebastian <lascauionutsebastian98@gmail.com>
2020-11-27 20:38:48 +0200

    solved merge button, the csv appear in interface and they can be cleared

--- app.py
+++ app.py
@@ -5,23 +5,34 @@ import glob
 from tkinter import filedialog as fd
 import shutil
 import pandas as pd
+from os import walk
 
 LARGE_FONT= ("Verdana", 12)
 NORM_FONT = ("Helvetica", 10)
 SMALL_FONT = ("Helvetica", 8)
 
+label_csvs = []
+
 def popup_msg(message):
-    popup = tk.Tk();
+    popup = tk.Toplevel()
     def leave():
         popup.destroy()
 
+    w = 300
+    h = 70
+    
+    sw = root.winfo_screenwidth()
+    sh = root.winfo_screenheight()
+    x = (sw - w)/ 2
+    y = (sh - h) / 2 - 50
+    popup.geometry('%dx%d+%d+%d' % (w, h, x, y))  
     popup.wm_title("Error")
     popup.iconbitmap(r"error_icon.ico")
     label = tk.Label(popup, text = message, font = NORM_FONT)
     label.pack(side = "top", fill = "x", pady = 10)
     B1 = tk.Button(popup, text = "Ok", command = popup.destroy)
     B1.pack()
-    popup.mainloop()
+    #popup.mainloop()
 
 
 def add_csv():
@@ -37,30 +48,70 @@ def add_csv():
         else:
             popup_msg('Please choose a file with csv extenstion!')
     #print('hello')
+    draw_input_files()
 
 def clear():
     if not os.path.exists('input_csv') or  not os.listdir('input_csv'):
         popup_msg('No csv files to process...')
     else:
-        # delete all csvs from csv_storage directory
+        # delete all csvs from input_csv directory
         filelist = glob.glob(os.path.join('input_csv', "*.csv"))
         for f in filelist:
             os.remove(f)
+    for label in label_csvs:
+        #print(label)
+        label.place_forget()
+    draw_input_files()
 
 def merge():
     if not os.path.exists('input_csv') or  not os.listdir('input_csv'):
         popup_msg('No csv files to merge...')
     else:
-        columns_list = ['test']
-        columns_names_list = ['ana']
+        files = []
+        for (dirpath, dirnames, filenames) in walk(os.getcwd() + '/input_csv'):
+            files.extend(filenames)
+        columns_list = []
+        columns_names_list = []
         current_directory = os.getcwd()
         output_directory = os.path.join(current_directory, r'output_csv')
         if not os.path.exists(output_directory):
             os.makedirs(output_directory)
-        data = pd.DataFrame(zip(*columns_list),
+
+        for file in files:
+            current_file_path = os.getcwd() + '/input_csv/' + file
+            data = pd.read_csv(current_file_path)
+            print(file)
+            for col in data.columns:
+                columns_names_list.append(col)
+                columns_list.append(data[col])
+
+        merged_data = pd.DataFrame(zip(*columns_list),
                             columns = columns_names_list)
-        data.to_csv('output_csv/output.csv')
+        merged_data.to_csv('output_csv/output.csv')
         os.startfile(os.getcwd() + '/output_csv')
+
+def add_row():
+    print('hello')
+
+def draw_input_files():
+    for widget in frame.winfo_children():
+        if isinstance(widget, tk.Label):
+            widget.destroy()
+    new_x, new_y = 250, 300
+    files = []
+    for (dirpath, dirnames, filenames) in walk(os.getcwd() + '/input_csv'):
+        files.extend(filenames)
+    for file in files:
+        new_y += 50
+        label = Label(root, text = '  -' + file, font = "50")
+        label.config(font=("Courier", 20))
+        label.place(x = new_x, y = new_y)
+        label_csvs.append(label)
+        current_file_path = os.getcwd() + '/input_csv/' + file
+        data = pd.read_csv(current_file_path)
+
+    #for col in data.columns:
+    #    print(
 
 root = Tk()
 root.title('Csv merger')
@@ -71,26 +122,41 @@ photo = PhotoImage(file = "icon_background.png")
 w = Label(root, image=photo)
 w.pack()
 '''
-root.geometry('750x500')
+root.geometry('750x500+120+120')
+
 
 frame = tk.Frame(root)
-frame.pack()
 
-add_button = tk.Button(frame,
+add_button = tk.Button(root,
                        text = 'Add csv',
                        command = add_csv)
-add_button.pack()
+add_button.grid(row=1, column=0)
 
-clear_button = tk.Button(frame,
+clear_button = tk.Button(root,
                          text = 'Clear',
                          command = clear)
-clear_button.pack()
+clear_button.grid(row=2, column=0)
 
-merge_button = tk.Button(frame,
+merge_button = tk.Button(root,
                          text = 'Merge',
-                         command = merge)
+                         command = merge,
+                         height = 5,
+                         width = 30)
 
-merge_button.pack()
+x_total = root.winfo_screenwidth()
+y_total = root.winfo_screenheight()
 
+merge_button.place(x = x_total / 2 - 100 , y = y_total / 2 + 350)
 
+w = Label(root, text ='Your input directory:', font = "50")
+w.config(font=("Courier", 30))
+w.place(x = 250, y = 300)
+
+draw_input_files()
+
+'''
+var2 = tk.IntVar()
+c2 = tk.Checkbutton(root, text='C++',variable=var2, onvalue=1, offvalue=0, command = add_row)
+c2.pack()
+'''
 root.mainloop()
